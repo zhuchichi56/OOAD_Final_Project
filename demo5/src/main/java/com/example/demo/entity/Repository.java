3401baf553456e79999c783c34a55697fad56d65
张艺严 <91365498+skylight615@users.noreply.github.com>
2022-10-02 20:03:55 +0800

    version1.2

--- demo5/src/main/java/com/example/demo/entity/Repository.java
+++ demo5/src/main/java/com/example/demo/entity/Repository.java
@@ -7,27 +7,27 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class repository {
+public class Repository {
     private int repoId;
     private String name;
     private int star;
     private int fork;
-    private int AgentId;
+    private int agentId;
     private int versionId;
     private String branch;
     private String content;
     private String comment;
-    private String commit_time;
+    private String commitTime;
 
-    public repository(String name, int star, int fork, int agentId, int versionId, String branch, String content, String comment, String commit_time) {
+    public Repository(String name, int star, int fork, int agentId, int versionId, String branch, String content, String comment, String commitTime) {
         this.name = name;
         this.star = star;
         this.fork = fork;
-        AgentId = agentId;
+        this.agentId = agentId;
         this.versionId = versionId;
         this.branch = branch;
         this.content = content;
         this.comment = comment;
-        this.commit_time = commit_time;
+        this.commitTime = commitTime;
     }
 }
