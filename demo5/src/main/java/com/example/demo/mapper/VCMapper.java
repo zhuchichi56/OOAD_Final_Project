5efdb57619febde319a461ff3b66e5c02a91b3bf
GT-111 <81619041+GT-111@users.noreply.github.com>
2022-10-03 21:42:19 +0800

    add Agent mapper

--- demo5/src/main/java/com/example/demo/mapper/VCMapper.java
+++ demo5/src/main/java/com/example/demo/mapper/VCMapper.java
@@ -1,10 +1,11 @@
 package com.example.demo.mapper;
 
+import com.example.demo.entity.VC;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface VCMapper {
     Integer getChildRepoId(@Param("fatherRepoId") int fatherRepoId);
-
+    int createNewVC(@Param("VC")VC vc);
 }
