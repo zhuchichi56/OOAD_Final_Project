cf578772147a0a22ffbbceed5e1de341aac281f5
张艺严 <91365498+skylight615@users.noreply.github.com>
2022-10-31 17:19:48 +0800

    新增文件覆写工具

---
+++ OOAD_Final_Project-main/demo5/src/main/java/com/example/demo/util/FileCoverUtil.java
@@ -0,0 +1,55 @@
+package com.example.demo.util;
+
+import java.io.*;
+import java.util.List;
+
+public class FileCoverUtil {
+
+    public static void updateFile(String path, File file){
+        File origin = new File(path);
+        deleteFile(origin);
+        File[] list = file.listFiles();
+        for (File f: list){
+            coverFile(path,f);
+        }
+    }
+    private static void deleteFile(File file){
+        if (file.getName().equals(".git")){
+            return;
+        }
+        if (file.isDirectory()){
+            File[] list = file.listFiles();
+            for (File f: list){
+                deleteFile(f);
+            }
+        }
+        file.delete();
+    }
+
+    private static void coverFile(String path, File file){
+        if (file.getName().equals(".git")){
+            return;
+        }
+        if (file.isDirectory()){
+            File[] list = file.listFiles();
+            String newPath = path+'\\'+file.getName();
+            File coverFile = new File(newPath);
+            boolean b = coverFile.mkdir();
+            for (File f: list){
+                coverFile(newPath,f);
+            }
+        } else {
+            try {
+                FileReader fr = new FileReader(file);
+                char[] buffer = new char[999999];
+                int len = fr.read(buffer);
+                FileWriter fw = new FileWriter(path+'\\'+file.getName());
+                fw.write(buffer);
+                fr.close();
+                fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
